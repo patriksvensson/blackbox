aef896048bfaca630a25ef71f085027628e4ff8b
Patrik Svensson <patrik@patriksvensson.se>
2011-01-13 12:34:25 +0100

    Added missing XML comments to LevelRangeFilter constructor.

--- src/BlackBox/Filters/LevelRangeFilter.cs
+++ src/BlackBox/Filters/LevelRangeFilter.cs
@@ -24,99 +24,102 @@ using System.Text;
 
 namespace BlackBox
 {
-    [LogFilterType("levelrange")]
-    public sealed class LevelRangeFilter : LogFilter
-    {
-        private LogLevel _minLevel;
-        private LogLevel _maxLevel;
-        private bool _minLevelHasBeenSet;
-        private bool _maxLevelHasBeenSet;
+	/// <summary>
+	/// Log filter that matches when the log level is within a specific range.
+	/// </summary>
+	[LogFilterType("levelrange")]
+	public sealed class LevelRangeFilter : LogFilter
+	{
+		private LogLevel _minLevel;
+		private LogLevel _maxLevel;
+		private bool _minLevelHasBeenSet;
+		private bool _maxLevelHasBeenSet;
 
-        /// <summary>
-        /// Gets or sets the minimum level.
-        /// </summary>
-        /// <value>The min level.</value>
-        public LogLevel MinLevel
-        {
-            get { return _minLevel; }
-            set
-            {
-                _minLevel = value;
-                _minLevelHasBeenSet = true;
-            }
-        }
+		/// <summary>
+		/// Gets or sets the minimum level.
+		/// </summary>
+		/// <value>The min level.</value>
+		public LogLevel MinLevel
+		{
+			get { return _minLevel; }
+			set
+			{
+				_minLevel = value;
+				_minLevelHasBeenSet = true;
+			}
+		}
 
-        /// <summary>
-        /// Gets or sets the maximum level.
-        /// </summary>
-        /// <value>The max level.</value>
-        public LogLevel MaxLevel
-        {
-            get { return _maxLevel; }
-            set
-            {
-                _maxLevel = value;
-                _maxLevelHasBeenSet = true;
-            }
-        }
+		/// <summary>
+		/// Gets or sets the maximum level.
+		/// </summary>
+		/// <value>The max level.</value>
+		public LogLevel MaxLevel
+		{
+			get { return _maxLevel; }
+			set
+			{
+				_maxLevel = value;
+				_maxLevelHasBeenSet = true;
+			}
+		}
 
-        /// <summary>
-        /// Gets or sets the action that will be used if the
-        /// log entry's log level is in the specified range.
-        /// </summary>
-        /// <value>The action.</value>
-        public LogFilterResult Action { get; set; }
+		/// <summary>
+		/// Gets or sets the action that will be used if the
+		/// log entry's log level is in the specified range.
+		/// </summary>
+		/// <value>The action.</value>
+		public LogFilterResult Action { get; set; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LevelRangeFilter"/> class.
-        /// </summary>
-        public LevelRangeFilter()
-        {
-            this.Action = LogFilterResult.Filter;
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LevelRangeFilter"/> class.
+		/// </summary>
+		public LevelRangeFilter()
+		{
+			this.Action = LogFilterResult.Filter;
+		}
 
-        /// <summary>
-        /// Initializes the log filter.
-        /// </summary>
-        /// <param name="locator">The locator.</param>
-        protected internal override void Initialize(IServiceLocator locator)
-        {
-            if (!_minLevelHasBeenSet)
-            {
-                throw new BlackBoxException("The minimum log level has not been set.");
-            }
-            if (!_maxLevelHasBeenSet)
-            {
-                throw new BlackBoxException("The maximum log level has not been set.");
-            }
-            if (_minLevel > _maxLevel)
-            {
-                throw new BlackBoxException("The minimum log level must be less or equal to the maximum log level.");
-            }
-        } 
+		/// <summary>
+		/// Initializes the log filter.
+		/// </summary>
+		/// <param name="locator">The locator.</param>
+		protected internal override void Initialize(IServiceLocator locator)
+		{
+			if (!_minLevelHasBeenSet)
+			{
+				throw new BlackBoxException("The minimum log level has not been set.");
+			}
+			if (!_maxLevelHasBeenSet)
+			{
+				throw new BlackBoxException("The maximum log level has not been set.");
+			}
+			if (_minLevel > _maxLevel)
+			{
+				throw new BlackBoxException("The minimum log level must be less or equal to the maximum log level.");
+			}
+		} 
 
-        /// <summary>
-        /// Evaluates the specified entry against the log filter.
-        /// </summary>
-        /// <param name="entry">The entry.</param>
-        /// <returns></returns>
-        protected internal override LogFilterResult Evaluate(ILogEntry entry)
-        {
-            if (entry != null)
-            {
-                if (entry.Level >= _minLevel && entry.Level <= _maxLevel)
-                {
-                    return this.Action;
-                }
-                else
-                {
-                    return LogFilterResult.Neutral;
-                }
-            }
-            else
-            {
-                return LogFilterResult.Neutral;
-            }
-        }
-    }
+		/// <summary>
+		/// Evaluates the specified entry against the log filter.
+		/// </summary>
+		/// <param name="entry">The entry.</param>
+		/// <returns></returns>
+		protected internal override LogFilterResult Evaluate(ILogEntry entry)
+		{
+			if (entry != null)
+			{
+				if (entry.Level >= _minLevel && entry.Level <= _maxLevel)
+				{
+					return this.Action;
+				}
+				else
+				{
+					return LogFilterResult.Neutral;
+				}
+			}
+			else
+			{
+				return LogFilterResult.Neutral;
+			}
+		}
+	}
 }
