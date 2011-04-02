1f739af6cc44bb97be7c4c48e74e1d78a0139ed8
Patrik <patrik@patriksvensson.se>
2011-04-02 20:39:18 +0200

    The equals-method expression now uses a default comparer.

--- src/BlackBox/Conditions/Expressions/EqualsMethodExpression.cs
+++ src/BlackBox/Conditions/Expressions/EqualsMethodExpression.cs
@@ -1,7 +1,27 @@
-﻿using System;
+﻿//
+// Copyright 2011 Patrik Svensson
+//
+// This file is part of BlackBox.
+//
+// BlackBox is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BlackBox is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser Public License for more details.
+//
+// You should have received a copy of the GNU Lesser Public License
+// along with BlackBox. If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace BlackBox.Conditions.Expressions
 {
@@ -20,16 +40,10 @@ namespace BlackBox.Conditions.Expressions
                 throw new ArgumentNullException("context");
             }
 
-            string actual = this.Arguments[0].Evaluate(context) as string;
-            string expected = this.Arguments[1].Evaluate(context) as string;
+            object leftResult = this.Arguments[0].Evaluate(context);
+            object rightResult = this.Arguments[1].Evaluate(context);
 
-            if (actual != null && expected != null)
-            {
-                // Check if the actual string equals the expected one.
-                return actual.Equals(expected, StringComparison.OrdinalIgnoreCase);
-            }
-
-            return false;
+            return Comparer.Default.Compare(leftResult, rightResult) == 0;
         }
     }
 }
